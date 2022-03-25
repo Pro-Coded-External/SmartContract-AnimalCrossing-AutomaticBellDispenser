aad964860e31caee6456a0c5bd56fe7d2c70e086
Matteo Leonesi <matteo.leonesi@studenti.unicam.it>
2022-03-25 18:44:04 +0100

    Revert "Merge pull request #1 from MatteoLeonesi/Nova"
    This reverts commit 87d2c7409e4e6a25a82b454a827230d4af4574fe, reversing
    changes made to 48629ad4b071161b91eb8aa284088d786879a0d6.

--- contracts/TomNookATM.sol
+++ contracts/TomNookATM.sol
@@ -8,14 +8,10 @@ contract TomNookATM {
     address payable TomNook;
     uint256 TomNook_Debts = 5696000; //biggest house
     uint256 deployDate;
-    IERC20 miles;
-    IERC20 bell;
 
-    constructor(address bellToken, address milesToken) {
+    constructor() {
         TomNook = payable(msg.sender);
         deployDate = block.timestamp;
-        miles = IERC20(milesToken);
-        bell = IERC20(bellToken);
     }
 
     struct Account {
