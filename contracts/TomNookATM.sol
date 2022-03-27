30e656b7052555fd2ee49a569c8d19ef4269e0a1
FoxDev12 <83777181+FoxDev12@users.noreply.github.com>
2022-03-27 13:00:30 +0200

    Update TomNookATM.sol
    -implemented interests
    -fixed payDebts
    -fixed BellVaucherDex

--- contracts/TomNookATM.sol
+++ contracts/TomNookATM.sol
@@ -24,6 +24,8 @@ contract TomNookATM {
         bool exists;
         address[] currentTokenAddresses;
         mapping(address => uint256) balances;
+        // We need this to calculate interests
+        uint256 timestampForInterests;
     }
 
     mapping(address => Account) private accounts;
@@ -48,31 +50,48 @@ contract TomNookATM {
         );
         _;
     }
+    // You don't need _tokenAddress if you don't let the user deposit when they create an account 
+    // function createAccount(address _tokenAddress)
+    //     external
+    //     onlyValidTokens(_tokenAddress)
+    // {
+    //     require(
+    //         !accounts[msg.sender].exists,
+    //         "TomNook ATM:: Already have an account"
+    //     );
+    //     // Since we now use a mapping to track the debt, initialization when the user creates an account
+    //     userDebt[msg.sender] = 5696000;
+    //     console.log("msg.sender : ", msg.sender);
+    //     accounts[msg.sender].exists = true;
+    // }
 
-    function createAccount(address _tokenAddress)
-        external
-        onlyValidTokens(_tokenAddress)
-    {
+    function createAccount() external {
         require(
             !accounts[msg.sender].exists,
             "TomNook ATM:: Already have an account"
         );
         // Since we now use a mapping to track the debt, initialization when the user creates an account
-        userDebt[msg.sender] == 5696000;
+        userDebt[msg.sender] = 5696000;
         console.log("msg.sender : ", msg.sender);
         accounts[msg.sender].exists = true;
+        accounts[msg.sender].timestampForInterests = block.timestamp;
     }
 
+
     //both account need to exist in the bbank system
+    // To avoid errors when calculating the interests, we need to make both the deposit and withdraw function claim the interests.
+    // Otherwise additional deposits / withdrawals would throw the rewards off. For the same reason, this is the first thing we need to do when depositing/withdrawing
     function deposit(address _tokenAddress, uint256 _amount)
         external
         accountExists
         onlyValidTokens(_tokenAddress)
     {
+        claimInterests();
         //tracking
         accounts[msg.sender].balances[_tokenAddress] += _amount;
         //transfer
         IERC20(_tokenAddress).transferFrom(msg.sender, address(this), _amount);
+        
     }
 
     function withdraw(address _tokenAddress, uint256 amount)
@@ -80,13 +99,16 @@ contract TomNookATM {
         accountExists
         onlyValidTokens(_tokenAddress)
     {
+        claimInterests();
         // If user tries to withdraw more than what he deposited then transfer all their balance.
         if (amount > accounts[msg.sender].balances[_tokenAddress]) {
             amount = accounts[msg.sender].balances[_tokenAddress];
         }
+        
         // Sets the amount BEFORE transferring out to prevent exploits (for more info, look up reentrancy attacks and the check-effect-interaction pattern)
         accounts[msg.sender].balances[_tokenAddress] -= amount;
         IERC20(_tokenAddress).transfer(msg.sender, amount);
+        
     }
 
     function getBalance(address _tokenAddress)
@@ -117,15 +139,24 @@ contract TomNookATM {
     }
 
     //animal crossing bank system has a stake system
-    function interests() external view {
+    function claimInterests() public {
+        // Do we want that? 
         //require(block.timestamp >= (deployDate + 30 days));
+
+        // assuming that you get interests on all your staked tokens, and not just a specific one. Lmk if that's not the case
+
+        // We're getting the time that passed since the user last claimed the interests, and multiply it by 100 for precision
+        uint256 rate = ((block.timestamp - accounts[msg.sender].timestampForInterests) * 100) / 1 days;
+
+            // for 30 days, rate would be 3000, 3000 / 600000 = 0.5 / 100
+        uint256 interests = (accounts[msg.sender].balances[address(bell)] * rate) / 600_000;
+        bell.transferFrom(TomNook, msg.sender, interests);
     }
 
     //pay debts to mr TomNook
-    function paydebts(address _tokenAddress, uint256 _amount)
+    function payDebts(uint256 _amount)
         external
         accountExists
-        onlyValidTokens(_tokenAddress)
     {
         if (userDebt[msg.sender] == 0) {
             console.log(
@@ -133,24 +164,30 @@ contract TomNookATM {
             );
         } else {
             //tracking
-            accounts[msg.sender].balances[_tokenAddress] -= _amount;
+            claimInterests();
+            accounts[msg.sender].balances[address(bell)] -= _amount;
             userDebt[msg.sender] -= _amount;
-            IERC20(_tokenAddress).transfer(TomNook, _amount);
+            bell.transfer(TomNook, _amount);
         }
     }
 
     //convert miles to bell (bell vaucher ), 500 miles -> 3000 bells
     function BellVaucherDex(
-        address _tokenAddressBels,
-        address _tokenAddressMiles,
+        // You don't really need those since the user can't actually choose
+        // also using _tokenAddressBels that way is super dangerous, i could make tomNook transfer another token to me instead of bells
+        // address _tokenAddressBels,
+        // address _tokenAddressMiles,
         uint256 _amount
-    ) external OnlyMilesForVaucher(_tokenAddressMiles) {
-        accounts[msg.sender].balances[_tokenAddressMiles] -= _amount;
-        IERC20(_tokenAddressBels).transferFrom(
+    ) external {
+        accounts[msg.sender].balances[address(miles)] -= _amount;
+        // Once again, it's either one or the other. If you add the amount to the players balance within the contract, you don't want to transfer the amount to him as well. 
+        // Otherwise you're effectively giving them 2 times the amount. 
+        // IERC20(_tokenAddressBels).transferFrom(
+        bell.transferFrom(
             TomNook,
             msg.sender,
             _amount * 6
         );
-        accounts[msg.sender].balances[_tokenAddressBels] += _amount * 6;
+        // accounts[msg.sender].balances[_tokenAddressBels] += _amount * 6;
     }
 }
